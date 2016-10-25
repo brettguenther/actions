8ece3632723c7d895ec16c4d448ca41b43234e2e
Brett <Brett@Bretts-MacBook-Air-2.local>
2016-10-25 13:47:19 -0400

    error clean

--- hello.py
+++ hello.py
@@ -19,7 +19,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template("action.html",
+    return render_template("index.html",
                            title='Home',
                            user=user,
                            posts=posts)
