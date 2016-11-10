1283aab2d43defc320f922069b5474487c01c0f9
Brett <Brett@Bretts-MacBook-Air-2.local>
2016-11-10 11:02:30 -0500

    new comments method

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, json
 app = Flask(__name__)
 import os
 
@@ -59,24 +59,61 @@ def ping_test():
     return "completed ping method"
 
 @app.route("/github_issue", methods=['GET', 'POST'])
-def github_isse():
+def github_issue():
     import requests
     import json
-    from base_functions import ConfigSectionMap
-    # github_info = ConfigSectionMap('/Users/Looker/looker_apps/actions/look_creds.ini','github')
-    # owner = github_info['github_user']
-    # github_base_url = github_info['github_base_url']
-    # auth_token = github_info['github_token']
     owner = os.environ['GITHUB_USER']
     github_base_url = os.environ['GITHUB_BASE_URL']
     auth_token = os.environ['GITHUB_TOKEN']
     repo = 'test'
+    # if request.method == 'POST':
+    #     data = request.data
+    #     dataDict = json.dumps(data)
+    #     if dataDict == '':
+    #         #this is the intitial form call to get back params
+    #
+    #     else:
+    #           #this is the case where the body contains contents of the form post
+    #
+    #
+    # else:
+    # from base_functions import ConfigSectionMap
+    # github_info = ConfigSectionMap('/Users/Looker/looker_apps/actions/look_creds.ini','github')
+    # owner = github_info['github_user']
+    # github_base_url = github_info['github_base_url']
+    # auth_token = github_info['github_token']
     label = request.args.get("label")
     labels_array = [label]
     issue_number = request.args.get("issue_number")
     github_issues_url = github_base_url + '/repos/{0}/{1}/issues'.format(owner,repo)
     github_labels_url = github_base_url + '/repos/{0}/{1}/issues/{2}/labels'.format(owner,repo,issue_number)
     r = requests.post(github_labels_url,auth=(owner, auth_token),data=json.dumps(labels_array))
+    return r.text
+
+@app.route("/github_form", methods=['GET', 'POST'])
+def github_form():
+    dat = [
+        {'Name': 'Title', 'required': true, 'default':'test title'},
+        {'Name': 'Comment', 'required': true, 'default':'test comment'}
+    ]
+    resp = Response(response=dat,status=200,mimetype="application/json")
+    return(resp)
+
+@app.route("/github_comment", methods=['GET', 'POST'])
+def github_comment():
+    import requests
+    import json
+    owner = os.environ['GITHUB_USER']
+    github_base_url = os.environ['GITHUB_BASE_URL']
+    auth_token = os.environ['GITHUB_TOKEN']
+    repo = 'test'
+    content = request.get_json()
+    post_comment = content['form_params']['comment']#need to extract comment from post body in looker form
+    print post_comment
+    issue_number = request.args.get("issue_number")
+    comment_body = {"body": post_comment}
+    github_issue_comments_url = github_base_url + '/repos/{0}/{1}/issues/{2}/comments'.format(owner,repo,issue_number)
+    r = requests.post(github_issue_comments_url,auth=(owner, auth_token),data=json.dumps(comment_body))
     return r.text
 
 if __name__ == "__main__":
